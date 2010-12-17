c98e4132c4d1ac9e0423c1d1c6954514cf4b94c1
Borja Sotomayor <borja@cs.uchicago.edu>
2010-12-16 19:02:29 -0600

    * Made the EC2 launcher multithreaded. * Recover gracefully from failures, and try to release EC2 resources whenever possible. * Many minor fixes.

--- lib/demogrid/ec2/launch.py
+++ lib/demogrid/ec2/launch.py
@@ -1,22 +1,37 @@
-from cPickle import dump, load, HIGHEST_PROTOCOL
-from boto.ec2.connection import EC2Connection
-from demogrid.common.utils import create_ec2_connection, SSH, MultiThread
+from cPickle import load
+from boto.exception import BotoClientError, EC2ResponseError
+from demogrid.common.utils import create_ec2_connection, SSH, MultiThread,\
+    DemoGridThread, SSHCommandFailureException, SIGINTWatcher
 import random
 import time
 import sys
+import traceback
 from demogrid.common import log
 
 
 class EC2Launcher(object):
-    def __init__(self, demogrid_dir, config, generated_dir, loglevel):
+    def __init__(self, demogrid_dir, config, generated_dir, loglevel, no_cleanup):
         self.demogrid_dir = demogrid_dir
         self.config = config
         self.generated_dir = generated_dir
         self.loglevel = loglevel
         self.conn = None
+        self.instances = None
+        self.vols = []
+        self.no_cleanup = no_cleanup
      
+    def run(self):
+        # This try-except will catch anything that isn't
+        # caught in launch() (which should handle all
+        # exceptions)
+        try:
+            self.launch()
+        except Exception, exc:
+            self.handle_unexpected_exception(exc)
+            
         
     def launch(self):     
+        SIGINTWatcher(self.cleanup_after_kill)
         t_start = time.time()
         
         ami = self.config.get_ami()
@@ -26,9 +41,20 @@ class EC2Launcher(object):
         
         log.init_logging(self.loglevel)
         
-        log.debug("Connecting to EC2...")
-        self.conn = create_ec2_connection()
-        log.debug("Connected to EC2.")
+        try:
+            log.debug("Connecting to EC2...")
+            self.conn = create_ec2_connection()
+            if self.conn == None:
+                print "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables are not set."
+                exit(1)
+            log.debug("Connected to EC2.")
+        except BotoClientError, exc:
+            print "\033[1;31mERROR\033[0m - Could not connect to EC2."
+            print "        Reason: %s" % exc.reason
+            exit(1)
+        except Exception, exc:
+            self.handle_unexpected_exception(exc)
+            
         
         # Load topology
         log.debug("Loading topology file...")
@@ -44,24 +70,37 @@ class EC2Launcher(object):
         if self.loglevel == 0:
             print "\033[1;37mLaunching %i EC2 instances...\033[0m" % num_instances,
             sys.stdout.flush()
-        log.info("Launching %i EC2 instances." % num_instances)        
-        reservation = self.conn.run_instances(ami, 
-                                         min_count=num_instances, 
-                                         max_count=num_instances,
-                                         instance_type=insttype,
-                                         security_groups= ["default"],
-                                         key_name=keypair,
-                                         placement = zone)
+
+        log.info("Launching %i EC2 instances." % num_instances)     
+        try:   
+            reservation = self.conn.run_instances(ami, 
+                                             min_count=num_instances, 
+                                             max_count=num_instances,
+                                             instance_type=insttype,
+                                             security_groups= ["default"],
+                                             key_name=keypair,
+                                             placement = zone)
+        except EC2ResponseError, exc:
+            self.handle_ec2response_exception(exc, "requesting instances")
+        except Exception, exc:
+            self.handle_unexpected_exception(exc)            
         
-        instances = reservation.instances
+        self.instances = reservation.instances
         
-        log.debug("Instances: %s" % " ".join([i.id for i in instances]))
+        log.debug("Instances: %s" % " ".join([i.id for i in self.instances]))
+        
         log.debug("Waiting for instances to start.")
         
-        mt = MultiThread(self.wait_instance, [[i] for i in instances])
-        mt.run()
+        mt_instancewait = MultiThread()
+        for i in self.instances:
+            mt_instancewait.add_thread(InstanceWaitThread(mt_instancewait, "wait-%s" % i.id, i, self))
+        mt_instancewait.run()
+        
+        if not mt_instancewait.all_success():
+            self.handle_mt_exceptions(mt_instancewait.get_exceptions(), "Exception raised while waiting for instances.")
             
-        print "\033[1;32mdone!\033[0m"            
+        if self.loglevel == 0:
+            print "\033[1;32mdone!\033[0m"            
         log.info("Instances are running.")
 
         # Kluge: This is because of a known bug in the stable release of boto.
@@ -69,7 +108,7 @@ class EC2Launcher(object):
         # the private IP, which we need), and the workaround is to force
         # boto to create a new Instance object.
         # Can be removed once they push a new stable release
-        r = self.conn.get_all_instances([i.id for i in instances])
+        r = self.conn.get_all_instances([i.id for i in self.instances])
         instances2 = r[0].instances
 
         node_instance = dict(zip(nodes,instances2))
@@ -90,15 +129,21 @@ class EC2Launcher(object):
         if self.loglevel == 0:
             print "\033[1;37mConfiguring DemoGrid nodes...\033[0m (this may take a few minutes)"
         log.info("Setting up DemoGrid on instances")        
-        mt = MultiThread(self.configure_instance, node_instance.items())
-        mt.run()      
         
+        mt_configure = MultiThread()
+        for node, instance in node_instance.items():
+            mt_configure.add_thread(InstanceConfigureThread(mt_configure, "configure-%s" % node.hostname.split(".")[0], node, instance, self))
+        mt_configure.run()        
+        
+        if not mt_configure.all_success():
+            self.handle_mt_exceptions(mt_configure.get_exceptions(), "DemoGrid was unable to configure the instances.")
+
         t_end = time.time()
         
         delta = t_end - t_start
         minutes = int(delta / 60)
         seconds = int(delta - (minutes * 60))
-        print "You just went \033[34mfrom zero to grid\033[0m in \033[1;37m%i minutes and %s seconds\033[0m!" % (minutes, seconds)
+        print "You just went \033[1;34mfrom zero to grid\033[0m in \033[1;37m%i minutes and %s seconds\033[0m!" % (minutes, seconds)
 
     def wait_state(self, obj, state, interval = 2.0):
         jitter = random.uniform(0.0, 0.5)
@@ -108,67 +153,163 @@ class EC2Launcher(object):
             if newstate == state:
                 return True
         # TODO: Check errors
-            
-    def wait_instance(self, instance):
-        self.wait_state(instance, "running")
-        log.info("Instance %s is running. Hostname: %s" % (instance.id,instance.public_dns_name))
+
+    def handle_ec2response_exception(self, exc, what=""):
+        if what != "": what = " when " + what
+        print "\033[1;31mERROR\033[0m - EC2 returned an error when %s." % what
+        print "        Reason: %s" % exc.reason
+        # Save body somewhere
+        self.cleanup()
+        exit(1)        
         
-    def configure_instance(self, node, instance):
+    def handle_unexpected_exception(self, exc, what=""):
+        if what != "": what = " when " + what
+        print "\033[1;31mERROR\033[0m - An unexpected '%s' exception has been raised" % what
+        print "        Message: %s" % exc
+        print "        Stack trace:"
+        traceback.print_exc()
+        self.cleanup()
+        exit(1)        
+
+    def handle_mt_exceptions(self, exceptions, what):
+        print "\033[1;31mERROR\033[0m - " + what
+        for name, exception in exceptions.items():
+            if isinstance(exception, SSHCommandFailureException):
+                print "        %s: Error while running '%s'" % (name, exception.command)
+            elif isinstance(exception, EC2ResponseError):
+                print "        %s: EC2 error '%s'" % (name, exception.reason)
+            else:          
+                print "        %s: Unexpected exception '%s'" % (name, exception.__class__.__name__)
+                print "        Message: %s" % exception
+            
+        self.cleanup()
+        exit(1)
+
+    def cleanup(self):
+        if self.no_cleanup:
+            print "--no-cleanup has been specified, so DemoGrid will not release EC2 resources."
+            print "Remember to do this manually"
+        else:
+            print "DemoGrid is attempting to release all EC2 resources..."
+            try:
+                if self.conn != None:
+                    for v in self.vols:
+                        if v.attachment_state == "attached":
+                            v.detach()
+                    if self.instances != None:
+                        self.conn.terminate_instances([i.id for i in self.instances])
+                    for v in self.vols:
+                        self.wait_state(v, "available")        
+                        v.delete()
+                    print "DemoGrid has released all EC2 resources."
+            except:
+                traceback.print_exc()
+                print "DemoGrid was unable to release all EC2 resources."
+                if self.instances != None:
+                    print "Please make sure the following instances have been terminated: " % [i.id for i in self.instances]
+                if len(self.vols) > 0:
+                    print "Please make sure the following volumes have been deleted: " % [v.id for v in self.vols]
+        
+    def cleanup_after_kill(self):
+        print "DemoGrid has been unexpectedly killed and cannot release EC2 resources."
+        print "Please make sure you manually release all DemoGrid instances and volumes."
+            
+class InstanceWaitThread(DemoGridThread):
+    def __init__(self, multi, name, instance, launcher):
+        DemoGridThread.__init__(self, multi, name)
+        self.instance = instance
+        self.launcher = launcher
+                    
+    def run2(self):
+        self.launcher.wait_state(self.instance, "running")
+        log.info("Instance %s is running. Hostname: %s" % (self.instance.id, self.instance.public_dns_name))
+        
+class InstanceConfigureThread(DemoGridThread):
+    def __init__(self, multi, name, node, instance, launcher):
+        DemoGridThread.__init__(self, multi, name)
+        self.node = node
+        self.instance = instance
+        self.launcher = launcher
+        
+    def run2(self):
+        node = self.node
+        instance = self.instance
+        
         log.info("Setting up instance %s. Hostname: %s" % (instance.id, instance.public_dns_name), node)
 
         log.debug("Creating Chef volume.", node)
-        vol = self.conn.create_volume(1, instance.placement, self.config.get_snap())
+        vol = self.launcher.conn.create_volume(1, instance.placement, self.launcher.config.get_snap())
         log.debug("Created Chef volume %s. Attaching." % vol.id, node)
         vol.attach(instance.id, '/dev/sdh')
         log.debug("Chef volume attached. Waiting for it to become in-use.", node)
-        self.wait_state(vol, "in-use")
+        self.launcher.wait_state(vol, "in-use")
         log.debug("Volume is in-use", node)
 
-        if self.loglevel in (0,1):
+        self.launcher.vols.append(vol)
+
+        self.check_continue()
+
+        if self.launcher.loglevel in (0,1):
             ssh_out = None
             ssh_err = None
-        elif self.loglevel == 2:
+        elif self.launcher.loglevel == 2:
             ssh_out = sys.stdout
             ssh_err = sys.stderr
 
         log.debug("Establishing SSH connection", node)
-        ssh = SSH("ubuntu", instance.public_dns_name, self.config.get_keyfile(), ssh_out, ssh_err)
+        ssh = SSH("ubuntu", instance.public_dns_name, self.launcher.config.get_keyfile(), ssh_out, ssh_err)
         ssh.open()
         log.debug("SSH connection established", node)
+
+        self.check_continue()
         
         log.debug("Mounting Chef volume", node)
         ssh.run("sudo mount -t ext3 /dev/sdh /chef")
         
         # Upload host file and update hostname
         log.debug("Uploading host file and updating hostname", node)
-        ssh.scp("%s/hosts_ec2" % self.generated_dir,
+        ssh.scp("%s/hosts_ec2" % self.launcher.generated_dir,
                 "/chef/cookbooks/demogrid/files/default/hosts")             
         ssh.run("sudo cp /chef/cookbooks/demogrid/files/default/hosts /etc/hosts")
         ssh.run("sudo bash -c \"echo %s > /etc/hostname\"" % node.hostname)
         ssh.run("sudo /etc/init.d/hostname restart")
         
+        self.check_continue()
+        
         # Upload topology file
         log.debug("Uploading topology file", node)
-        ssh.scp("%s/topology_ec2.rb" % self.generated_dir,
+        ssh.scp("%s/topology_ec2.rb" % self.launcher.generated_dir,
                 "/chef/cookbooks/demogrid/attributes/topology.rb")             
         
         # Copy certificates
         log.debug("Copying certificates", node)
-        ssh.scp_dir("%s/certs" % self.generated_dir, 
+        ssh.scp_dir("%s/certs" % self.launcher.generated_dir, 
                     "/chef/cookbooks/demogrid/files/default/")
+        
+        self.check_continue()
+
+        if self.launcher.loglevel == 0:
+            print "   \033[1;37m%s\033[0m: Basic setup is done. Installing Grid software now." % node.hostname.split(".")[0]
         
         # Run chef
         log.debug("Running chef", node)
-        ssh.scp("%s/lib/ec2/chef.conf" % self.demogrid_dir,
+        ssh.scp("%s/lib/ec2/chef.conf" % self.launcher.demogrid_dir,
                 "/tmp/chef.conf")        
         
         ssh.run("echo '{ \"run_list\": \"role[%s]\" }' > /tmp/chef.json" % node.role)
 
         ssh.run("sudo chef-solo -c /tmp/chef.conf -j /tmp/chef.json")    
+
+        self.check_continue()
+        
+        ssh.run("sudo umount /chef")
+        vol.detach()
+        self.launcher.wait_state(vol, "available")        
+        vol.delete()
         
         ssh.run("sudo update-rc.d nis enable")     
 
         log.info("Configuration done.", node)
         
-        if self.loglevel == 0:
-            print "   \033[1;37m%s\033[0m has been configured." % node.hostname.split(".")[0]
+        if self.launcher.loglevel == 0:
+            print "   \033[1;37m%s\033[0m is ready." % node.hostname.split(".")[0]
