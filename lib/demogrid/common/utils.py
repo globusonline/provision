5eace5c3e012941e6eafd23cf71262d6fbc3e535
Borja Sotomayor <borja@cs.uchicago.edu>
2010-12-16 19:03:48 -0600

    Added classes for multithread support.

--- lib/demogrid/common/utils.py
+++ lib/demogrid/common/utils.py
@@ -6,6 +6,7 @@ Created on Dec 6, 2010
 
 import threading
 import paramiko
+import operator
 import traceback
 import select
 import sys
@@ -14,29 +15,129 @@ from boto.ec2.connection import EC2Connection
 from os import walk, environ
 import socket    
 from demogrid.common import log
+import os
+import signal
         
+class ThreadAbortException(Exception):
+    pass
         
-class MultiThread(object):
-    def __init__(self, func, funcargs):
-        self.num_threads = len(funcargs)
-        self.done_threads = 0
-        self.func = func
-        self.threads = [threading.Thread(target = self.run_func, args = args) for args in funcargs]    
-        self.semaphore = threading.Semaphore()
-        self.done = threading.Event()
+class DemoGridThread (threading.Thread):
+    def __init__ (self, multi, name):
+        threading.Thread.__init__(self)
+        self.multi = multi
+        self.name = name
+        self.exception = None
+        self.status = -1
+        
+    def check_continue(self):
+        if self.multi.abort.is_set():
+            raise ThreadAbortException()
+        
+    def run2(self):
+        pass
+        
+    def run(self):
+        try:
+            self.run2()
+            self.status = 0
+        except Exception, e:
+            self.exception = e
+            self.status = 1
+            self.multi.thread_failure(self)
+            
+        if self.status == 0:
+            self.multi.thread_success(self)
 
-    def run_func(self, *args):
-        self.func(*args)
-        with self.semaphore:
-            self.done_threads += 1
-            if self.done_threads == self.num_threads:
-                self.done.set()
+class MultiThread(object):
+    def __init__(self):
+        self.num_threads = 0
+        self.done_threads = 0
+        self.threads = {}
+        self.lock = threading.Lock()
+        self.all_done = threading.Event()
+        self.abort = threading.Event()
+
+    def add_thread(self, thread):
+        self.threads[thread.name] = thread     
+        self.num_threads += 1
 
     def run(self):
         self.done_threads = 0
-        for t in self.threads:
+        for t in self.threads.values():
             t.start()
-        self.done.wait()
+        self.all_done.wait()
+        
+    def thread_success(self, thread):
+        with self.lock:
+            self.done_threads += 1
+            if self.done_threads == self.num_threads:
+                self.all_done.set()            
+
+    def thread_failure(self, thread):
+        with self.lock:
+            if not isinstance(thread.exception, ThreadAbortException):
+                self.abort.set()
+            else:
+                thread.status = 2
+            self.done_threads += 1
+            if self.done_threads == self.num_threads:
+                self.all_done.set()           
+                
+    def all_success(self):
+        return all([t.status == 0 for t in self.threads.values()])
+        
+    def get_exceptions(self):
+        return dict([(t.name, t.exception) for t in self.threads.values() if t.status == 1]) 
+
+# From http://code.activestate.com/recipes/496735-workaround-for-missed-sigint-in-multithreaded-prog/
+# Modified so it will run a cleanup function
+class SIGINTWatcher(object):
+    """this class solves two problems with multithreaded
+    programs in Python, (1) a signal might be delivered
+    to any thread (which is just a malfeature) and (2) if
+    the thread that gets the signal is waiting, the signal
+    is ignored (which is a bug).
+
+    The watcher is a concurrent process (not thread) that
+    waits for a signal and the process that contains the
+    threads.  See Appendix A of The Little Book of Semaphores.
+    http://greenteapress.com/semaphores/
+
+    I have only tested this on Linux.  I would expect it to
+    work on the Macintosh and not work on Windows.
+    """
+    
+    def __init__(self, cleanup_func):
+        """ Creates a child thread, which returns.  The parent
+            thread waits for a KeyboardInterrupt and then kills
+            the child thread.
+        """
+        self.cleanup_func = cleanup_func
+        self.child = os.fork()
+        if self.child == 0:
+            return
+        else:
+            self.watch()
+
+    def watch(self):
+        try:
+            os.wait()
+        except KeyboardInterrupt:
+            self.cleanup_func()
+            self.kill()
+        sys.exit()
+
+    def kill(self):
+        try:
+            os.kill(self.child, signal.SIGKILL)
+        except OSError: pass
+        
+
+class SSHCommandFailureException(Exception):
+    def __init__(self, ssh, command):
+        self.ssh = ssh
+        self.command = command
+        
         
 class SSH(object):
     def __init__(self, username, hostname, key_path, default_outf = sys.stdout, default_errf = sys.stderr, port=22):
@@ -69,7 +170,7 @@ class SSH(object):
     def close(self):
         self.client.close()
         
-    def run(self, command, outf=None, errf=None):
+    def run(self, command, outf=None, errf=None, exception_on_error = True):
         channel = self.client.get_transport().open_session()
         
         log.debug("%s - Running %s" % (self.hostname,command))
@@ -105,14 +206,15 @@ class SSH(object):
             rc = channel.recv_exit_status()
             log.debug("%s - Ran %s" % (self.hostname,command))
             channel.close()
+        except Exception, e:
+            raise # Replace by something more meaningful
+         
+        if exception_on_error and rc != 0:
+            raise SSHCommandFailureException(self, command)
+        else:
             return rc
     
-        except Exception, e:
-            traceback.print_exc()
-            try:
-                channel.close()
-            except:
-                pass      
+
         
         
     def scp(self, fromf, tof):
