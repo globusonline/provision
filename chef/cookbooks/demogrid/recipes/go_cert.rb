e50557bf84c708f66f833e21bd767648e5d3bfb0
Borja Sotomayor <borja@cs.uchicago.edu>
2011-05-19 20:41:41 -0500

    Added recipe to install GO MyProxy CA.

---
+++ chef/cookbooks/demogrid/recipes/go_cert.rb
@@ -0,0 +1,37 @@
+# -------------------------------------------------------------------------- #
+# Copyright 2010, University of Chicago                                      #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+# -------------------------------------------------------------------------- #
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# RECIPE: GO CA certificate
+#
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+cookbook_file "/etc/grid-security/certificates/4396eb4d.0" do
+  source "4396eb4d.0"
+  mode 0644
+  owner "root"
+  group "root"
+end
+
+cookbook_file "/etc/grid-security/certificates/4396eb4d.signing_policy" do
+  source "4396eb4d.signing_policy"
+  mode 0644
+  owner "root"
+  group "root"
+end
