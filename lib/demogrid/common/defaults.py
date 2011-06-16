02a1b6a6d2d1aa282d9b3fbaf0804e7ac241924c
Borja Sotomayor <borja@cs.uchicago.edu>
2011-06-16 19:22:49 +0200

    Added a default location for the instance "data store".

--- lib/demogrid/common/defaults.py
+++ lib/demogrid/common/defaults.py
@@ -2,3 +2,4 @@ import os.path
 
 CONFIG_FILE = os.path.expanduser("~/.demogrid/demogrid.conf")
 GENERATED_LOCATION = os.path.expanduser("~/.demogrid/generated")
+INSTANCE_LOCATION = os.path.expanduser("~/.demogrid/instances")
