145141f14570ee84ecf563391c39d91f633b60f6
Borja Sotomayor <borja@cs.uchicago.edu>
2011-06-17 19:34:09 +0200

    Added base classes for deployers

---
+++ lib/demogrid/core/deploy.py
@@ -0,0 +1,16 @@
+'''
+Created on Jun 17, 2011
+
+@author: borja
+'''
+
+class Deployer(object):
+    def __init__(self, demogrid_dir, no_cleanup, extra_files):
+        self.demogrid_dir = demogrid_dir
+        self.instance = None
+        self.extra_files = extra_files
+        self.no_cleanup = no_cleanup
+        
+class VM(object):
+    def __init__(self):
+        pass
