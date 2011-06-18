0304cb2743e0bcf2b6e35007f74eca14d6dc1880
Borja Sotomayor <borja@cs.uchicago.edu>
2011-06-18 18:53:23 +0200

    Make no_cleanup and extra_files optional, as they're only needed when starting VMs, not when stopping them.

--- lib/demogrid/core/deploy.py
+++ lib/demogrid/core/deploy.py
@@ -5,7 +5,7 @@ Created on Jun 17, 2011
 '''
 
 class Deployer(object):
-    def __init__(self, demogrid_dir, no_cleanup, extra_files):
+    def __init__(self, demogrid_dir, no_cleanup = False, extra_files = []):
         self.demogrid_dir = demogrid_dir
         self.instance = None
         self.extra_files = extra_files
