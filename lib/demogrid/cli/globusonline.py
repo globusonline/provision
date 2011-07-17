55a307828c8e058d9a40f314193a296257a97743
Borja Sotomayor <borja@cs.uchicago.edu>
2011-07-17 10:34:48 -0500

    Update demogrid-go-register-endpoint to use the instance store

--- lib/demogrid/cli/globusonline.py
+++ lib/demogrid/cli/globusonline.py
@@ -7,23 +7,14 @@ from demogrid.cli import Command
 from demogrid.common import defaults, constants
 from demogrid.core.config import DemoGridConfig
 from demogrid.globusonline.transfer_api import TransferAPIClient, ClientError
+from demogrid.core.instance import InstanceStore
 
 class demogrid_go_register_endpoint(Command):
     
     name = "demogrid-go-register-endpoint"
     
     def __init__(self, argv):
-        Command.__init__(self, argv)
-        
-        self.optparser.add_option("-c", "--conf", 
-                                  action="store", type="string", dest="conf", 
-                                  default = defaults.CONFIG_FILE,
-                                  help = "Configuration file.")        
-        
-        self.optparser.add_option("-g", "--generated-dir", 
-                                  action="store", type="string", dest="dir", 
-                                  default = defaults.GENERATED_LOCATION,
-                                  help = "Directory with generated files.")        
+        Command.__init__(self, argv)    
 
         self.optparser.add_option("-d", "--domain", 
                                   action="store", type="string", dest="domain", 
@@ -43,19 +34,18 @@ class demogrid_go_register_endpoint(Command):
                 
     def run(self):    
         self.parse_options()
+        inst_id = self.args[1]
         
-        config = DemoGridConfig(self.opt.conf)
-        go_username = config.get_go_username()
-        go_cert_file, go_key_file = config.get_go_credentials()
-        go_server_ca = config.get_go_server_ca()
+        istore = InstanceStore(self.opt.dir)
+        inst = istore.get_instance(inst_id)        
+
+        go_username = inst.config.get_go_username()
+        go_cert_file, go_key_file = inst.config.get_go_credentials()
+        go_server_ca = inst.config.get_go_server_ca()
 
         api = TransferAPIClient(go_username, go_server_ca, go_cert_file, go_key_file)
-        
-        f = open ("%s/topology.dat" % self.opt.dir, "r")
-        topology = load(f)
-        f.close()            
-        
-        domain = topology.domains[self.opt.domain]
+                
+        domain = inst.topology.domains[self.opt.domain]
         ep_name = self.opt.name
                 
         # Find the GridFTP server
@@ -72,7 +62,7 @@ class demogrid_go_register_endpoint(Command):
                 print ce
                 exit(1)
         
-        if config.get_go_auth() == "go": 
+        if inst.config.get_go_auth() == "go": 
             myproxy_server = "myproxy.globusonline.org"
         else:
             myproxy_server = gridftp.org.auth.hostname
