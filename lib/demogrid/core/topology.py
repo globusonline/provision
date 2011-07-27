e79c58ba4234a56142bc46769bacef58af8c36a9
Borja Sotomayor <borja@cs.uchicago.edu>
2011-07-26 19:30:33 -0500

    Started re-implementing support for the old (simpler) configuration file, which will be necessary for instructional users.

--- lib/demogrid/core/topology.py
+++ lib/demogrid/core/topology.py
@@ -210,91 +210,80 @@ ff02::3 ip6-allhosts
     def from_configfile(cls, config):
         topology = cls()
         
-        grid_auth_node = None
-        if config.has_grid_auth_node():
-            grid_auth_node = DGNode(role = self.GRID_AUTH_ROLE,
-                                    ip =   self.__gen_IP(self.GRID_MANAGEMENT_SUBNET, self.GRID_AUTH_HOST),
-                                    hostname = self.__gen_hostname(self.GRID_AUTH_HOSTNAME))
-            grid.add_node(grid_auth_node)
-        
-        org_subnet = self.ORG_SUBNET_START
-        for org_name in self.config.organizations:
-            org = DGOrganization(org_name, org_subnet)
-            grid.add_organization(org)
+        for domain_name in config.domains:
+            domain = Domain(domain_name)
+            topology.add_domain(domain)
             
-            if self.config.has_org_users_file(org_name):
-                usersfile = self.config.get_org_users_file(org_name)
+            if config.has_org_users_file(domain_name):
+                usersfile = config.get_org_users_file(domain_name)
                 usersfile = open(usersfile, "r")
                 
                 for line in usersfile:
                     fields = line.split()
                     type = fields[0]
                     username = fields[1]
-                    password = fields[2]
-                    password_hash = fields[3]
+                    password_hash = fields[2]
                     
                     if type == "G":
-                        user = DGOrgUser(login = username,
-                                         description = "User '%s' of Organization %s" % (username, org_name),
-                                         gridenabled = True, password = password, password_hash = password_hash,
-                                         auth_type = self.config.get_org_user_auth(org_name))
+                        cert_type = config.get_org_user_auth(domain_name)
                     else:
-                        user = DGOrgUser(login = username,
-                                         description = "User '%s' of Organization %s" % (username, org_name),
-                                         gridenabled = False, password = password, password_hash = password_hash)
-                    org.add_user(user)
+                        cert_type = None
+                    
+                    user = User(login = username,
+                                description = "User '%s' of Organization %s" % (username, domain_name),
+                                password_hash = password_hash,
+                                cert_type = cert_type)
+                        
+                    domain.add_user(user)
                     
                 usersfile.close()
             else:
                 usernum = 1
-                for i in range(self.config.get_org_num_gridusers(org_name)):
-                    username = "%s-user%i" % (org_name, usernum)
-                    user = DGOrgUser(login = username,
-                                     description = "User '%s' of Organization %s" % (username, org_name),
-                                     gridenabled = True, password = self.DEFAULT_USER_PASSWD, password_hash = self.DEFAULT_USER_PASSWDHASH,
-                                     auth_type = self.config.get_org_user_auth(org_name))
-                    org.add_user(user)
+                for i in range(config.get_org_num_gridusers(domain_name)):
+                    username = "%s-user%i" % (domain_name, usernum)
+                    user = User(login = username,
+                                description = "User '%s' of Organization %s" % (username, domain_name),
+                                password_hash = "TODO",
+                                cert_type = config.get_org_user_auth(domain_name))
+                    domain.add_user(user)
                     usernum += 1
     
-                for i in range(self.config.get_org_num_nongridusers(org_name)):
-                    username = "%s-user%i" % (org_name, usernum)
-                    user = DGOrgUser(login = "%s-user%i" % (org_name, usernum),
-                                     description = "User '%s' of Organization %s" % (username, org_name),
-                                     gridenabled = False, password = self.DEFAULT_USER_PASSWD, password_hash = self.DEFAULT_USER_PASSWDHASH)
-                    org.add_user(user)
+                for i in range(config.get_org_num_nongridusers(domain_name)):
+                    username = "%s-user%i" % (domain_name, usernum)
+                    user = User(login = username,
+                                description = "User '%s' of Organization %s" % (username, domain_name),
+                                password_hash = "TODO",
+                                cert_type = None)
+                    domain.add_user(user)
                     usernum += 1
                 
             
-            server_node = DGNode(role = self.SERVER_ROLE,
-                                 ip =   self.__gen_IP(org_subnet, self.SERVER_HOST),
-                                 hostname = self.__gen_hostname(self.SERVER_HOSTNAME, org = org_name),
-                                 org = org)            
-            grid.add_org_node(org, server_node)
-            org.server = server_node
+            server_node = Node(name = "%s-nfsnis" % domain_name, domain = domain)
+            server_node.run_list.append("role[domain-nfsnis]")
+                        
+            topology.add_domain_node(domain, server_node)
+            domain.servers[constants.DOMAIN_NIS_SERVER] = server_node
+            domain.servers[constants.DOMAIN_NFS_SERVER] = server_node
             
-            if self.config.has_org_login(org_name):            
-                login_node =  DGNode(role = self.LOGIN_ROLE,
-                                     ip =   self.__gen_IP(org_subnet, self.LOGIN_HOST),
-                                     hostname = self.__gen_hostname(self.LOGIN_HOSTNAME, org = org_name),
-                                     org = org)                        
-                grid.add_org_node(org, login_node)
+            if config.has_domain_login(domain_name):            
+                login_node = Node(name = "%s-login" % domain_name, domain = domain)
+                login_node.run_list.append("role[domain-login]")
+                                 
+                topology.add_domain_node(domain, login_node)
 
-            if self.config.has_org_auth(org_name):
-                auth_node = DGNode(role = self.MYPROXY_ROLE,
-                                   ip =   self.__gen_IP(org_subnet, self.MYPROXY_HOST),
-                                   hostname = self.__gen_hostname(self.MYPROXY_HOSTNAME, org = org_name),
-                                   org = org)                        
-                grid.add_org_node(org, auth_node)
-                org.auth = auth_node
-            else:
-                org.auth = grid_auth_node
+            if config.get_org_user_auth(domain_name) == "myproxy":
+                myproxy_node = Node(name = "%s-myproxy" % domain_name, domain = domain)
+                myproxy_node.run_list.append("role[domain-myproxy]")
+                                 
+                topology.add_domain_node(domain, myproxy_node)
+                domain.servers[constants.DOMAIN_MYPROXY_SERVER] = myproxy_node
 
-            if self.config.has_org_gridftp(org_name):
-                gridftp_node = DGNode(role = self.GRIDFTP_ROLE,
-                                      ip =   self.__gen_IP(org_subnet, self.GRIDFTP_HOST),
-                                      hostname = self.__gen_hostname(self.GRIDFTP_HOSTNAME, org = org_name),
-                                      org = org)                        
-                grid.add_org_node(org, gridftp_node)
+            if config.has_org_gridftp(domain_name):
+                gridftp_node = Node(name = "%s-gridftp" % domain_name, domain = domain)
+                gridftp_node.run_list.append("role[domain-gridftp]")
+                                 
+                topology.add_domain_node(domain, gridftp_node)
+                domain.servers[constants.DOMAIN_GRIFTP_SERVER] = gridftp_node
             
             if self.config.has_org_lrm(org_name):
                 lrm_type = self.config.get_org_lrm(org_name)
@@ -330,31 +319,7 @@ ff02::3 ip6-allhosts
                     
                     clusternode_host += 1
             
-            org_subnet += 1
-        
-        # Generate Chef attributes
-        nodes = grid.get_nodes()
-        for node in nodes:        
-            attrs = node.attrs
-            attrs["demogrid_hostname"] = "\"%s\"" % node.hostname
-            attrs["node_id"] = "\"%s\"" % node.node_id
-            attrs["run_list"] = "[ \"role[%s]\" ]" % node.role
-            if node.org != None:
-                attrs["org"] = "\"%s\"" % node.org.name
-                if node.org.auth != None:
-                    attrs["auth"] = "\"%s\"" % self.__gen_hostname("auth", node.org.name)
-                if self.config.has_org_lrm(node.org.name):
-                    attrs["lrm_head"] = "\"%s\"" % self.__gen_hostname("gatekeeper", node.org.name)
-                    attrs["lrm_nodes"] = "%i" % self.config.get_org_num_clusternodes(node.org.name)
-        
-        nodes = grid.get_nodes()
-        for n in nodes:
-            if n.org != None:
-                attrs = n.attrs
-                attrs["subnet"] = "\"%s\"" % self.__gen_IP(n.org.subnet, 0)
-                attrs["org_server"] = "\"%s\"" % self.__gen_IP(n.org.subnet, 1)
-            
-        return grid
+        return topology
 
 
     @classmethod
