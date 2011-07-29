faefdaf54372549c9aa4bccfbc3e2be15bb05a05
Borja Sotomayor <borja@cs.uchicago.edu>
2011-07-29 18:25:25 -0500

    Create a GO endpoint for all domains in a topology using a consistent naming scheme (but still allow for a user to specify a specific name for a single domain)

--- lib/demogrid/cli/globusonline.py
+++ lib/demogrid/cli/globusonline.py
@@ -45,51 +45,57 @@ class demogrid_go_register_endpoint(Command):
 
         api = TransferAPIClient(go_username, go_server_ca, go_cert_file, go_key_file)
                 
-        domain = inst.topology.domains[self.opt.domain]
-        ep_name = self.opt.name
-                
-        # Find the GridFTP server
-        gridftp = domain.servers[constants.DOMAIN_GRIFTP_SERVER]
-        
-        
-        try:
-            (code, msg, ep) = api.endpoint(ep_name)
-            ep_exists = True
-        except ClientError as ce:
-            if ce.status_code == 404:
-                ep_exists = False
-            else:
-                print ce
-                exit(1)
-        
-        if inst.config.get_go_auth() == "go": 
-            myproxy_server = "myproxy.globusonline.org"
-        else:
-            myproxy_server = domain.servers[constants.DOMAIN_GRIFTP_SERVER].hostname
-        
-        if ep_exists:
-            if not self.opt.replace:
-                print "An endpoint called '%s' already exists. Please choose a different name." % ep_name
-                exit(1)
-            else:
-                (code, msg, data) = api.endpoint_delete(ep_name)
-            
-        (code, msg, data) = api.endpoint_create(ep_name, gridftp.hostname, description="DemoGrid endpoint",
-                                                scheme="gsiftp", port=2811, subject="/O=Grid/OU=DemoGrid/CN=host/%s" % gridftp.hostname,
-                                                myproxy_server=myproxy_server)
-        if code >= 400:
-            print code, msg
-            exit(1)        
+        for domain_name, domain in inst.topology.domains.items():
+            ep_name = "%s_%s" % (inst_id, domain_name)
 
-        if self.opt.public:
-            (code, msg, ep) = api.endpoint(ep_name)
-            if code >= 400:
-                print code, msg
-                exit(1)
-            ep["public"] = True
-            (code, msg, data) = api.endpoint_update(ep)
-            if code >= 400:
-                print code, msg
-                exit(1)
+            if self.opt.domain != None:
+                if self.opt.domain != domain_name:
+                    continue
+                else:
+                    if self.opt.name != None:
+                        ep_name = self.opt.name
                 
-        print "Endpoint created successfully"
+            # Find the GridFTP server
+            gridftp = domain.servers[constants.DOMAIN_GRIFTP_SERVER]
+
+            try:
+                (code, msg, ep) = api.endpoint(ep_name)
+                ep_exists = True
+            except ClientError as ce:
+                if ce.status_code == 404:
+                    ep_exists = False
+                else:
+                    print ce
+                    exit(1)
+            
+            if inst.config.get_go_auth() == "go": 
+                myproxy_server = "myproxy.globusonline.org"
+            else:
+                myproxy_server = domain.servers[constants.DOMAIN_MYPROXY_SERVER].hostname
+            
+            if ep_exists:
+                if not self.opt.replace:
+                    print "An endpoint called '%s' already exists. Please choose a different name." % ep_name
+                    exit(1)
+                else:
+                    (code, msg, data) = api.endpoint_delete(ep_name)
+                
+            (code, msg, data) = api.endpoint_create(ep_name, gridftp.hostname, description="DemoGrid endpoint",
+                                                    scheme="gsiftp", port=2811, subject="/O=Grid/OU=DemoGrid/CN=host/%s" % gridftp.hostname,
+                                                    myproxy_server=myproxy_server)
+            if code >= 400:
+                print code, msg
+                exit(1)        
+    
+            if self.opt.public:
+                (code, msg, ep) = api.endpoint(ep_name)
+                if code >= 400:
+                    print code, msg
+                    exit(1)
+                ep["public"] = True
+                (code, msg, data) = api.endpoint_update(ep)
+                if code >= 400:
+                    print code, msg
+                    exit(1)
+                    
+            print "Created endpoint '%s#%s' for domain '%s'" % (go_username, ep_name, domain_name)
