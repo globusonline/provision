b440831fea2642c4cbd98ae55365f1fcf79d7419
Borja Sotomayor <borja@borjanet.com>
2011-08-07 20:12:36 -0500

    Beginnings of topology documentation generator.

---
+++ doc/gen_topology_doc.py
@@ -0,0 +1,42 @@
+from demogrid.common.persistence import *
+import textwrap
+
+
+def print_section(title, marker):
+    print title
+    print marker * len(title)
+
+print_section("Topology", "*")
+
+def gen_cls_doc(cls):
+    print_section("``%s``" % cls.__name__, "=")
+    
+    doc = cls.__doc__
+    if doc != None:
+        doc = textwrap.dedent(doc).strip()
+    else:
+        doc = "TODO"    
+    
+    p_names = cls.properties.keys()
+    p_names.sort()
+    
+    for p_name in p_names:
+        p = cls.properties[p_name]
+        
+        description = textwrap.dedent(p.description).strip()
+        #print "**%s**" % p_name
+        print_section(p_name,"-")
+        print
+        print description
+        print
+
+gen_cls_doc(Topology)
+gen_cls_doc(Domain)
+gen_cls_doc(Node)
+gen_cls_doc(User)
+gen_cls_doc(GridMapEntry)
+gen_cls_doc(GOEndpoint)
+gen_cls_doc(DeployData)
+gen_cls_doc(EC2DeployData)
+
+
