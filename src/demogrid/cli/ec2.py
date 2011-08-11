30a1dccc9e29ee9af4cfe56dfba6ba17eacf1786
Borja Sotomayor <borja@cs.uchicago.edu>
2011-08-11 16:50:56 -0500

    Remove demogrid-ec2-create-volume

--- src/demogrid/cli/ec2.py
+++ src/demogrid/cli/ec2.py
@@ -4,36 +4,11 @@ Created on Jun 16, 2011
 @author: borja
 '''
 from demogrid.cli import Command
-from demogrid.deploy.ec2.images import EC2ChefVolumeCreator, EC2AMICreator,\
-    EC2AMIUpdater
+from demogrid.deploy.ec2.images import EC2AMICreator, EC2AMIUpdater
 from demogrid.common import defaults
 from demogrid.core.config import DemoGridConfig
 from demogrid.common.utils import parse_extra_files_files
 
-class demogrid_ec2_create_chef_volume(Command):
-    
-    name = "demogrid-ec2-create-chef-volume"
-    
-    def __init__(self, argv):
-        Command.__init__(self, argv)
-        
-        self.optparser.add_option("-a", "--ami", 
-                                  action="store", type="string", dest="ami", 
-                                  help = "AMI to use to create the volume.")
-
-        self.optparser.add_option("-k", "--keypair", 
-                                  action="store", type="string", dest="keypair", 
-                                  help = "EC2 keypair")
-        
-        self.optparser.add_option("-f", "--keypair-file", 
-                                  action="store", type="string", dest="keyfile", 
-                                  help = "EC2 keypair file")
-                
-    def run(self):    
-        self.parse_options()
-        
-        c = EC2ChefVolumeCreator(self.dg_location, self.opt.ami, self.opt.keypair, self.opt.keyfile)
-        c.run()  
         
 
 class demogrid_ec2_create_ami(Command):
