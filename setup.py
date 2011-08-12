bd4f3335a55d0c3fa8305b53d666326549391b7e
Borja Sotomayor <borja@cs.uchicago.edu>
2011-08-11 19:18:26 -0500

    Converted setup.py from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,34 +1,49 @@
 from os import walk
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-data_files = []
-for root, dirs, files in walk("chef/"):
-    if len(files) > 0:
-        data_files.append(("share/demogrid/" + root,[root + "/" + f for f in files]))
+import inspect
 
-data_files += [('share/demogrid/etc/', ["etc/demogrid.conf.sample",
-                                        "etc/uvb.template"]),
-               ('share/demogrid/lib/', ["lib/chef-node.sh",
-                                        "lib/create_from_master_img.sh"]),
-               ('share/demogrid/lib/uvb', ["lib/uvb/files-chefserver.txt",
-                                           "lib/uvb/post-install-chefserver.sh"]),
-              ]
+import demogrid.cli.api
+import demogrid.cli.ec2
+import demogrid.cli.globusonline
 
-setup(name='demogrid',
-      version='0.1.0',
-      description='DemoGrid',
+def gen_entrypoints(module):
+    members = dict([(name,cmd) for name, cmd in inspect.getmembers(module)])
+    cmd_names = [name for name, cmd in members.items() 
+                 if inspect.isclass(cmd) and issubclass(cmd, demogrid.cli.Command) and cmd != demogrid.cli.Command]
+    eps = []
+    for name in cmd_names:
+        if "%s_func" % name in members:
+            cmd = members[name]
+            eps.append("%s = %s:%s_func" % (cmd.name, module.__name__, name))
+
+    return eps
+
+setup(name='globus-provision',
+      version='0.3.0rc1',
+      description='Globus Provision',
       author='University of Chicago',
       author_email='borja@cs.uchicago.edu',
-      url='',
-      package_dir = {'': 'lib'},
-      packages=['demogrid'],
-      scripts=['bin/demogrid-clone-image', 
-               'bin/demogrid-prepare', 
-               'bin/demogrid-register-host-chef', 
-               'bin/demogrid-register-host-libvirt'],
-      data_files=data_files,
+      url='http://globus.org/provision',
+      package_dir = {'': 'src'},      
+      packages=find_packages("src", exclude=["dg_paraproxy"]),
+      
+      install_requires = ['boto>=2.0', 'pycrypto>=2.3', 'paramiko>=1.7.7.1'],
+      setup_requires = [ "setuptools_git >= 0.4.2", ],
+      include_package_data=True,
+      
+      entry_points = {
+        'console_scripts': gen_entrypoints(demogrid.cli.api) + 
+                           gen_entrypoints(demogrid.cli.ec2) +
+                           gen_entrypoints(demogrid.cli.globusonline)
+        },
+
+      package_data = {"chef":["*"]},
+      zip_safe = False,
+      #eager_resources=["chef/"],
+      license="Apache Software License",
       classifiers=[
-          'Development Status :: 3 - Alpha',
+          'Development Status :: 4 - Beta',
           'Environment :: Console',
           'Intended Audience :: Developers',
           'Intended Audience :: System Administrators',
