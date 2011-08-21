1f83d96e91d5d6ea4fb9529fecc08ddb4d78e35f
Borja Sotomayor <borja@cs.uchicago.edu>
2011-08-21 16:54:57 -0500

    Added license notices.

--- src/globus/provision/chef-files/cookbooks/globus/recipes/client-tools.rb
+++ src/globus/provision/chef-files/cookbooks/globus/recipes/client-tools.rb
@@ -1,5 +1,5 @@
 # -------------------------------------------------------------------------- #
-# Copyright 2010-2011, University of Chicago                                      #
+# Copyright 2010-2011, University of Chicago                                 #
 #                                                                            #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may    #
 # not use this file except in compliance with the License. You may obtain    #
