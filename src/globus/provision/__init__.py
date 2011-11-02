e7f99341ee3530f2fdf2eaadc5dabbaa632f4bdf
Borja Sotomayor <borja@cs.uchicago.edu>
2011-11-02 18:40:53 -0500

    Version and AMI bump

--- src/globus/provision/__init__.py
+++ src/globus/provision/__init__.py
@@ -21,9 +21,9 @@ See http://globus.org/provision/ for more details
 """
 
 VERSION="0.4"
-RELEASE="0.4.0a1"
+RELEASE="0.4.0a2"
 AMI={"us-east-1":
-        {"32-bit": "ami-4f35f826",
-         "64-bit": "ami-375d905e",
-         "HVM": "ami-0b5d9062"}
+        {"32-bit": "ami-b12ee0d8",
+         "64-bit": "ami-652ce20c",
+         "HVM": "ami-6d2ce204"}
      }
